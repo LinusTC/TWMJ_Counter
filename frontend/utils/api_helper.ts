ec30895d0b9309e2b63d6ff2977ae49d1407200b
LinusTC <tongchun03@gmail.com>
2025-12-09 01:46:44 +0800

    added some stuff

--- frontend/utils/api_helper.ts
+++ frontend/utils/api_helper.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const twmj_api = axios.create({
-    baseURL: "https://1090c2bbf090.ngrok-free.app",
+    baseURL: "https://36c0afc42eaa.ngrok-free.app",
 })
 
 export default twmj_api;
